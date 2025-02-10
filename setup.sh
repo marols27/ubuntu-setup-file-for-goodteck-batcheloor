41a0d0e88e4aac54a143e71e03ac2fcd98cc2812
Martin Repvik Olsbø <43169864+marols27@users.noreply.github.com>
2025-02-10 14:20:30 +0100

    Update setup.sh
    Each command should now auto confirm using -y flags when necesary.

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
 # Ros2 setup:
+# admin permition for the script: remember <<EOF [CONTENT] EOF enclosing arround the whole script!!!
+yes | sudo bash -c <<EOF
 
 locale  # check for UTF-8
 sudo apt update && sudo apt install locales
@@ -6,16 +8,16 @@ sudo locale-gen en_US en_US.UTF-8
 sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
 export LANG=en_US.UTF-8
 locale  # verify settings
-sudo apt install software-properties-common
-sudo add-apt-repository universe
+sudo apt install software-properties-common -y
+sudo add-apt-repository universe -y
 sudo apt update && sudo apt install curl -y
 sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
-sudo apt update && sudo apt install ros-dev-tools
+sudo apt update && sudo apt install ros-dev-tools -y
 sudo apt update
-sudo apt upgrade
-sudo apt install ros-jazzy-desktop
-sudo apt install ros-jazzy-ros-base
+sudo apt upgrade -y
+sudo apt install ros-jazzy-desktop -y
+sudo apt install ros-jazzy-ros-base -y
 
 # Replace ".bash" with your shell if you're not using bash
 # Possible values are: setup.bash, setup.sh, setup.zsh
@@ -24,7 +26,7 @@ source /opt/ros/jazzy/setup.bash
 
 
 # Gazebo setup:
-sudo apt-get install ros-${ROS_DISTRO}-ros-gz
+sudo apt install ros-${ROS_DISTRO}-ros-gz -y
 
 echo 'export LIBGL_ALWAYS_SOFTWARE=1' >> ~/.bashrc
 echo 'export QT_QPA_PLATFORM=xcb' >> ~/.bashrc
@@ -46,9 +48,11 @@ source /opt/ros/jazzy/setup.bash
 
 # Universal Robots package download:
 cd ~/ros2_ws/src
-git clone -b ros2 https://github.com/UniversalRobots/Universal_Robots_ROS2_GZ_Simulation.git src/ur_simulation_gz
+git clone -b ros2 https://github.com/UniversalRobots/Universal_Robots_ROS2_GZ_Simulation.git ~/ros2_ws/src/ur_simulation_gz
 rosdep install --from-paths src --ignore-src -r -y
 cd ..
 colcon build --symlink-install
 source ~/ros2_ws/install/setup.bash
 
+
+EOF
